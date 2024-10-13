97b6d90af5f4135d5647ee5e1117257a7c06fba7
John <cebotari@riseup.net>
2024-10-13 10:59:29 +0300

    create method for enroll, display students

--- lab2/lab2/Faculty.java
+++ lab2/lab2/Faculty.java
@@ -4,18 +4,52 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Faculty implements Serializable{
+public class Faculty implements Serializable {
 
     private String name;
     private String abbreviation;
     private List<Student> students;
-    private StudyField StudyField;
+    private StudyField studyField;
 
-    public Faculty(String name, String abbreviation, StudyField StudyField, List<Student> students){
+    public Faculty(String name, String abbreviation, StudyField studyField, List<Student> students) {
         this.name = name;
         this.abbreviation = abbreviation;
-        this.StudyField = StudyField;
+        this.studyField = studyField;
         this.students = new ArrayList<>();
     }
 
+    public void enrollStudent(Student student) {
+
+        students.add(student);
+    }
+
+    public void displayCurrentStudents() {
+
+        System.out.println("Currently Enrolled Students in " + abbreviation + ": ");
+
+        for (Student student : students) {
+
+            System.out.println(student.getFirstName() + " " + student.getLastName());
+        }
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public List<Student> getStudents() {
+
+        return students;
+    }
+
+    public String getAbbreviation() {
+
+        return abbreviation;
+    }
+
+    public StudyField getStudyField() {
+
+        return studyField;
+    }
 }
